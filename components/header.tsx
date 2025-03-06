d11dc057ae965e6936e40ed590b8ab343bf8dd1f
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-03-06 16:49:18 +0530

    added links for the header

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,43 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import UserMenu from "@/components/user-menu";
+import { PenBox } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            alt="task-flow logo"
+            width={200}
+            height={56}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive">
+              <PenBox size={18} />
+              <span> Create project</span>
+            </Button>
+          </Link>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 

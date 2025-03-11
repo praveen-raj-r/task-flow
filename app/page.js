730306616918fb900080fd92359e2a6deb5cd405
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-03-11 12:03:34 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -77,10 +77,10 @@ export default function Home() {
     <div className="min-h-screen">
       {/* Hero Section */}
       <section className="container mx-auto py-20 text-center">
-        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
+        <h1 className="text-5xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
           Streamline Your Workflow <br />
+          with
           <span className="flex mx-auto gap-3 sm:gap-4 items-center">
-            with
             <Image
               src={"/logo2.png"}
               alt="Task Flow Logo"
@@ -90,20 +90,24 @@ export default function Home() {
             />
           </span>
         </h1>
-        <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
+
+        <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto px-4">
           Empower your team with our intuitive project management solution.
         </p>
-        <p className="text-xl mb-12 max-w-2xl mx-auto"></p>
-        <Link href="/onboarding">
-          <Button size="lg" className="mr-4">
-            Get Started <ChevronRight size={18} className="ml-1" />
-          </Button>
-        </Link>
-        <Link href="#features">
-          <Button size="lg" variant="outline">
-            Learn More
-          </Button>
-        </Link>
+
+        <div className="flex justify-center items-center gap-3 sm:flex-row flex-col">
+          <Link href="/onboarding">
+            <Button size="lg">
+              Get Started <ChevronRight size={18} className="ml-1" />
+            </Button>
+          </Link>
+
+          <Link href="#features">
+            <Button size="lg" variant="outline">
+              Learn More
+            </Button>
+          </Link>
+        </div>
       </section>
 
       {/* Features Section */}
